6f5cbae228a08fedc3ef34c8d8bb9d716da1957c
Woipot <frostik0409@gmail.com>
2018-10-09 09:11:52 +0300

    Добавьте файлы проекта.

---
+++ Kr_1_patern/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kr_1_patern
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
